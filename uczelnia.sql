5f5415a41b4af8f5b54d7366a2a5090b8bc4345b
xSebus1 <zs0154155@gmail.com>
2025-11-05 15:21:51 +0100

    Update uczelnia.sql

--- uczelnia.sql
+++ uczelnia.sql
@@ -11,7 +11,8 @@ SELECT etat, COUNT(*) AS liczba_pracownikow FROM pracownicy GROUP BY etat;
 SELECT etat FROM pracownicy GROUP BY etat HAVING AVG(placa_pod) > 1400;
 SELECT etat FROM pracownicy ORDER BY COUNT(*) DESC;
 SELECT id_zesp, AVG(placa_pod) AS srednia_pensja FROM pracownicy GROUP BY id_zesp;
-SELECT id_zesp, MAX(placa_pod) AS max_pensja FROM pracownicy GROUP BY id_zesp;
+SELECT id_zesp, MAX(placa_pod) AS max_pensja FROM pracownicy GROUP BY etat;
+SELECT imie, nazwisko from pracownicy group by year(data_zatrudnienia);
 SELECT * FROM pracownicy WHERE placa_pod = (SELECT MIN(placa_pod) FROM pracownicy);
 SELECT * FROM pracownicy WHERE nazwisko LIKE 'K%';
 SELECT * FROM `pracownicy` WHERE placa_pod > 3000
@@ -21,3 +22,5 @@ SELECT * FROM pracownicy WHERE nazwisko like "k%i";
 SELECT id_zesp FROM pracownicy GROUP BY id_zesp HAVING COUNT(*) > 3;
 ELECT id_zesp FROM pracownicy GROUP BY id_zesp HAVING COUNT(*) > 3;
 SELECT id_zesp FROM pracownicy GROUP BY id_zesp ORDER BY COUNT(*) DESC
+SELECT imie, nazwisko FROM pracownicy WHERE placa_pod = (SELECT MIN(placa_pod) from pracownicy);
+SELECT id_zesp, COUNT(*) AS l_pracownikow FROM pracownicy GROUP BY id_zesp ORDER BY l_pracownikow DESC LIMIT 1;
